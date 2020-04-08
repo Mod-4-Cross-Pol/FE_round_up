af26b303585d87703478521396fa1e4b1ebbd499
Zachary Nemeroff <zanemeroff@gmail.com>
2020-04-08 11:23:35 -0600

    Refactor styling

--- components/ShortEventCard/ShortEventCard.js
+++ components/ShortEventCard/ShortEventCard.js
@@ -1,67 +1,35 @@
 import React from 'react';
-import { StyleSheet, View, Image, Text, ScrollView } from 'react-native';
+import { StyleSheet, View, Text} from 'react-native';
 
-export default function ShortEventCard() {
+export default function ShortEventCard(props) {
   return (
-    <View>
-      <ScrollView style={styles.scrollView}>
-        <View style={styles.smallCardContainer}>
-          <Text style={styles.titleText}>Volleyball</Text>
-          <Text style={styles.text}>Location: Wash Park</Text>
-          <Text style={styles.text}>Time: 2:00PM</Text>
-        </View>
-        <View style={styles.smallCardContainer}>
-          <Text style={styles.titleText}>Ultimate Frisbee</Text>
-          <Text style={styles.text}>Location: Cheeseman Park</Text>
-          <Text style={styles.text}>Time: 4:30PM</Text>
-        </View>
-        <View style={styles.smallCardContainer}>
-          <Text style={styles.titleText}>Dodgeball</Text>
-          <Text style={styles.text}>Location: Confuluence Park</Text>
-          <Text style={styles.text}>Time: 6:45PM</Text>
-        </View>
-        <View style={styles.smallCardContainer}>
-          <Text style={styles.titleText}>Backyard Croquet</Text>
-          <Text style={styles.text}>Location: Sloan's Lake Park</Text>
-          <Text style={styles.text}>Time: 10:00AM</Text>
-        </View>
-        <View style={styles.smallCardContainer}>
-          <Text style={styles.titleText}>Beer Olympics</Text>
-          <Text style={styles.text}>Location: City Park</Text>
-          <Text style={styles.text}>Time: 3:00PM</Text>
-        </View>
-      </ScrollView>
+    <View key={Math.random()} style={styles.smallCardContainer}>
+      <Text style={styles.titleText}>{props.activity}</Text>
+      <Text style={styles.text}>{props.location}</Text>
+      <Text style={styles.text}>Time: {props.time}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  navText: {
-    fontSize: 35,
-    textAlign: 'center',
-    marginTop: 15
-  },
-  scrollView: {
-    width: 'auto',
-    position: 'relative',
-    marginBottom: 50,
-    top: 40,
-    height: 300
-  },
   smallCardContainer: {
     height: 100,
     width: 'auto',
     marginBottom: 20,
-    backgroundColor: '#3b4aff',
-    borderRadius: 10,
-    paddingLeft: 10
+    backgroundColor: '#28BFBD',
+    borderRadius: 50,
+    alignItems: 'center',
+    borderWidth: 3,
+    borderColor: '#3b4aff'
   },
   titleText: {
+    fontFamily: 'Farah',
     fontSize: 30,
-    color: '#FFF'
+    color: '#000',
+    marginTop: 7
   },
   text: {
     fontSize: 20,
-    color: '#FFF'
+    color: '#000'
   }
 });
