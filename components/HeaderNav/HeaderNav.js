56f2155e6d949da9f58e84993200eb52ca1829c7
Zachary Nemeroff <zanemeroff@gmail.com>
2020-04-08 11:22:38 -0600

    Add onpress for navigation

--- components/HeaderNav/HeaderNav.js
+++ components/HeaderNav/HeaderNav.js
@@ -1,13 +1,16 @@
 import React from 'react';
+// import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import CreateEventForm from '../CreateEventForm/CreateEventForm';
 import { StyleSheet, View, Text, Button } from 'react-native';
 
-export default function HeaderNav() {
+export default function HeaderNav({ navigation }) {
   return (
     <View style={styles.nav}>
       <View style={styles.navButtonContainer}>
-        <View style={styles.navButton}><Button title='change date'/></View>
-        <View style={styles.navButton}><Button title='create activity'/></View>
-        <View style={styles.navButton}><Button title='log out'/></View>
+        <View style={styles.navButton}><Button title='CHANGE DATE'/></View>
+        <View style={styles.navButton}><Button onPress={() => navigation.navigate('CreateEventForm')}  title='CREATE EVENT'/></View>
+        <View style={styles.navButton}><Button title='LOG OUT'/></View>
       </View>
     </View>
   );
@@ -16,18 +19,12 @@ export default function HeaderNav() {
 const styles = StyleSheet.create({
   nav: {
     width: '100%',
-    height: 140,
-    backgroundColor: '#9cffad'
+    height: 120,
+    backgroundColor: '#292c33'
   },
   navText: {
     fontSize: 35,
     textAlign: 'center',
     marginTop: 70
-  },
-  navButtonContainer: {
-    marginTop: 25
-  },
-  navButton: {
-
   }
 });
