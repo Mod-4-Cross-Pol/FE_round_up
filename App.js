d8082dd63798fbfb8dca8af63261f3d331757944
Zachary Nemeroff <zanemeroff@gmail.com>
2020-04-08 11:21:46 -0600

    Create navigation screens

--- App.js
+++ App.js
@@ -1,28 +1,27 @@
 import 'react-native-gesture-handler';
-import 'react-native-gesture-handler';
 import * as React from 'react';
 // import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import LoginView from './components/LoginView/LoginView';
 import Map from './components/Map/Map';
 import HeaderNav from './components/HeaderNav/HeaderNav';
 import ShortEventCard from './components/ShortEventCard/ShortEventCard';
 import FullEventCard from './components/FullEventCard/FullEventCard';
 import CreateEventForm from './components/CreateEventForm/CreateEventForm';
+import Dashboard from './containers/Dashboard/Dashboard'
 import { StyleSheet, View, Image, Text, ScrollView } from 'react-native';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-
-    <HeaderNav />
-    <Map />
-    <ShortEventCard />
-
-
-
-      </View>
+      <Stack.Navigator initialRouteName='LoginView'>
+        <Stack.Screen name='Login' component={LoginView} />
+        <Stack.Screen name='Dashboard' component={Dashboard} />
+        <Stack.Screen name='CreateEventForm' component={CreateEventForm} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
@@ -45,7 +44,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#28BFBD',
-    alignItems: 'center',
-    // justifyContent: 'center'
+    alignItems: 'center'
   }
 });
+
+//   '#292c33'
+//   '#3b4aff'
+//   '#28BFBD'
