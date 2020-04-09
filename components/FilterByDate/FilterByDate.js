6309ba5e77c5f8d86171f28b393121aabdd23031
Zachary Nemeroff <zanemeroff@gmail.com>
2020-04-09 17:38:42 -0600

    Fix indent

--- components/FilterByDate/FilterByDate.js
+++ components/FilterByDate/FilterByDate.js
@@ -5,13 +5,13 @@ import ModalDropdown from 'react-native-modal-dropdown';
 export default function FilterByDate() {
   return (
     <View style={styles.dropdownContainer}>
-    <ModalDropdown
-      textStyle={styles.dropdownText}
-      dropdownTextStyle={styles.dropdownTextStyle}
-      defaultValue='By date -'
-      options={['6:00AM', '7:00AM', '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM', '7:00PM', '8:00PM', '9:00PM','10:00PM' ]}
-    />
-  </View>
+      <ModalDropdown
+        textStyle={styles.dropdownText}
+        dropdownTextStyle={styles.dropdownTextStyle}
+        defaultValue='By date -'
+        options={['6:00AM', '7:00AM', '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM', '7:00PM', '8:00PM', '9:00PM','10:00PM' ]}
+      />
+    </View>
   );
 }
 
