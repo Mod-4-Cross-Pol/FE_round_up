d7da879dc8bcc76d050e575f55e124df93301dd9
Zachary Nemeroff <zanemeroff@gmail.com>
2020-04-09 10:12:06 -0600

    Install and implement dropdown modal package

--- components/CreateEventForm/CreateEventForm.js
+++ components/CreateEventForm/CreateEventForm.js
@@ -1,51 +1,120 @@
-import React from 'react';
-import { StyleSheet, View, Button, Text, TextInput} from 'react-native';
+import React, { useState } from 'react';
+import ModalDropdown from 'react-native-modal-dropdown';
+import { StyleSheet, View, Button, Text, TextInput, Alert } from 'react-native';
 
 export default function CreateEventForm() {
+
+  const [ nameOfSport, setNameOfSport ] = useState('')
+
+  const validateInputs = () => {
+    Alert.alert(nameOfSport)
+  }
+
+  const handleNameOfSportChange = e => {
+  setNameOfSport(e.target.value);
+  }
+
+  //  ** Name of Sport should be a dropdown
+
   return (
     <View style={styles.container}>
-      <View style={styles.closeButton}><Button title='X'/></View>
-      <Text style={styles.text}>CREATE A NEW EVENT FORM!</Text>
-      <TextInput style={styles.input} placeholder='Event Name...'/>
+      <Text style={styles.text}>CREATE A NEW EVENT</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Name of Sport...'
+        onChangeText={handleNameOfSportChange}
+        value={nameOfSport}
+      />
       <TextInput style={styles.input} placeholder='Location...'/>
-      <TextInput style={styles.input} placeholder='Start Time...'/>
-      <TextInput style={styles.input} placeholder='End Time...'/>
-      <TextInput style={styles.input} placeholder='Total Players Required...'/>
-      <TextInput style={styles.input} placeholder='Players Curretly Attending...'/>
-      <TextInput style={styles.input} placeholder='Skill Level...'/>
+      <TextInput style={styles.input} placeholder='Date...'/>
+
+      <View style={styles.timeContainer}>
+        <View style={styles.dropdownContainer}>
+          <Text style={styles.text}>Start Time:</Text>
+          <ModalDropdown
+            textStyle={styles.dropdownText}
+            dropdownTextStyle={styles.dropdownTextStyle}
+            defaultValue='>> select time <<'
+            options={['6:00AM', '7:00AM', '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM', '7:00PM', '8:00PM', '9:00PM','10:00PM' ]}
+          />
+        </View>
+        <View style={styles.dropdownContainer}>
+          <Text style={styles.text}>End Time:</Text>
+          <ModalDropdown
+            textStyle={styles.dropdownText}
+            dropdownTextStyle={styles.dropdownTextStyle}
+            defaultValue='>> select time <<'
+            options={['6:00AM', '7:00AM', '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM', '7:00PM', '8:00PM', '9:00PM','10:00PM' ]}
+          />
+        </View>
+      </View>
+
+      <TextInput style={styles.input} keyboardType={'numeric'} placeholder='Total Players Required...'/>
+      <TextInput style={styles.input} keyboardType={'numeric'} placeholder='Players Currently Attending...'/>
+
+      <View style={styles.dropdownContainer}>
+        <Text style={styles.text}>Skill Level:</Text>
+        <ModalDropdown
+          textStyle={styles.dropdownText}
+          dropdownTextStyle={styles.dropdownTextStyle}
+          defaultValue='>> select skill level <<'
+          options={['just for fun', 'beginner', 'intermediate', 'advanced']}
+        />
+      </View>
+
       <TextInput style={styles.input} placeholder='Equipment Reqired...'/>
       <TextInput style={styles.input} placeholder='Notes...'/>
-      <Button title='create event!'/>
+      <View style={styles.createEventButton}><Button onPress={validateInputs} title='create event!'/></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: '80%',
-    height: '85%',
-    backgroundColor: '#3b4aff',
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#28BFBD',
     alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10
+    justifyContent: 'center'
   },
   text: {
     fontSize: 20,
     marginVertical: 5,
-    color: '#FFF'
+    color: '#FFF',
+    fontWeight: 'bold'
   },
   input: {
     height: 50,
-    width: 300,
+    width: '90%',
     backgroundColor: '#fff',
-    borderRadius: 10,
+    borderRadius: 25,
     textAlign: 'center',
     fontSize: 20,
     marginTop: 15
   },
-  closeButton: {
-    position: 'absolute',
-    top: 5,
-    right: 5
+  timeContainer: {
+    flexDirection: 'row',
+    marginTop: 10
+  },
+  dropdownContainer: {
+    alignItems: 'center',
+    height: 70,
+    width: '50%'
+  },
+  dropdownText: {
+    fontSize: 20,
+    backgroundColor: '#fff'
+  },
+  dropdownTextStyle: {
+    fontSize: 20,
+    width: 150
+  },
+  createEventButton: {
+    backgroundColor: '#FFF',
+    marginTop: 20,
+    height: 50,
+    width: '50%',
+    borderRadius: 25,
+    paddingTop: 5
   }
 });
