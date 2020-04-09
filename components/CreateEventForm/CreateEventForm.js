26067f9bc26f89c1c117d7151899602fcd8169c7
Zachary Nemeroff <zanemeroff@gmail.com>
2020-04-09 17:36:49 -0600

    Add state via hooks to all components

--- components/CreateEventForm/CreateEventForm.js
+++ components/CreateEventForm/CreateEventForm.js
@@ -2,59 +2,106 @@ import React, { useState } from 'react';
 import ModalDropdown from 'react-native-modal-dropdown';
 import { StyleSheet, View, Button, Text, TextInput, Alert } from 'react-native';
 
-export default function CreateEventForm() {
+export default function CreateEventForm(props) {
 
-  const [ nameOfSport, setNameOfSport ] = useState('')
+  const [ nameOfActivity, setNameOfActivity ] = useState('')
+  const [ location, setLocation ] = useState('')
+  const [ startTime, setStartTime ] = useState('')
+  const [ endTime, setEndTime ] = useState('')
+  const [ playersRequired, setPlayersRequired ] = useState('')
+  const [ currentlyAttending, setCurrentlyAttending ] = useState('')
+  const [ skillLevel, setSkillLevel ] = useState('')
+  const [ equipmentRequired, setEquipmentRequired ] = useState('')
+  const [ notes, setNotes ] = useState('')
 
   const validateInputs = () => {
-    Alert.alert(nameOfSport)
+
   }
 
-  const handleNameOfSportChange = e => {
-  setNameOfSport(e.target.value);
+  const verifyEndTimeIsAfterStartTime = () => {
+
   }
 
-  //  ** Name of Sport should be a dropdown
+  const verifyFieldsAreNotEmpty = () => {
+    if ( !nameOfActivity || !location || !startTime || !endTime ||
+         !playersRequired || !currentlyAttending || !skillLevel ||
+         !equipmentRequired ) {
+      return false
+    } else {
+      return true
+    }
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>CREATE A NEW EVENT</Text>
+
+      <View style={styles.dropdownContainer}>
+        <Text style={styles.text}>Name of Activity:</Text>
+        <ModalDropdown
+          renderButtonText={nameOfActivity => setNameOfActivity(nameOfActivity)}
+          textStyle={styles.dropdownText}
+          dropdownTextStyle={styles.dropdownTextStyle}
+          defaultValue='>> select activity <<'
+          options={['Volleyball', 'Spikeball', 'Baseball', 'Basketball', 'Horseshoes', 'Beer Olympics']}
+        />
+      </View>
+
       <TextInput
         style={styles.input}
-        placeholder='Name of Sport...'
-        onChangeText={handleNameOfSportChange}
-        value={nameOfSport}
+        placeholder='Location...'
+        onChangeText={location => setLocation(location)}
+        value={location}
       />
-      <TextInput style={styles.input} placeholder='Location...'/>
-      <TextInput style={styles.input} placeholder='Date...'/>
+
+      <TextInput style={styles.input} placeholder='Date...(NEEDS DATEPICKER)'/>
 
       <View style={styles.timeContainer}>
         <View style={styles.dropdownContainer}>
           <Text style={styles.text}>Start Time:</Text>
           <ModalDropdown
+            renderButtonText={startTime => setStartTime(startTime)}
             textStyle={styles.dropdownText}
             dropdownTextStyle={styles.dropdownTextStyle}
             defaultValue='>> select time <<'
-            options={['6:00AM', '7:00AM', '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM', '7:00PM', '8:00PM', '9:00PM','10:00PM' ]}
+            options={['6:00AM', '6:30AM', '7:00AM', '7:30AM', '8:00AM', '8:30AM', '9:00AM', '9:30AM', '10:00AM', '10:30AM','11:00AM', '11:30AM', '12:00PM', '12:30PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM', '7:00PM', '8:00PM', '9:00PM','10:00PM' ]}
           />
         </View>
         <View style={styles.dropdownContainer}>
           <Text style={styles.text}>End Time:</Text>
           <ModalDropdown
+            renderButtonText={endTime => setEndTime(endTime)}
             textStyle={styles.dropdownText}
             dropdownTextStyle={styles.dropdownTextStyle}
             defaultValue='>> select time <<'
+            disabled={true}
             options={['6:00AM', '7:00AM', '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM', '7:00PM', '8:00PM', '9:00PM','10:00PM' ]}
           />
         </View>
       </View>
 
-      <TextInput style={styles.input} keyboardType={'numeric'} placeholder='Total Players Required...'/>
-      <TextInput style={styles.input} keyboardType={'numeric'} placeholder='Players Currently Attending...'/>
+      <TextInput
+        style={styles.input}
+        keyboardType={'numeric'}
+        placeholder='Total Players Required...'
+        onChangeText={playersRequired => setPlayersRequired(playersRequired)}
+        value={playersRequired}
+        keyboardType={'numeric'}
+      />
+
+      <TextInput
+        style={styles.input}
+        keyboardType={'numeric'}
+        placeholder='Players Currently Attending...'
+        onChangeText={currentlyAttending => setCurrentlyAttending(currentlyAttending)}
+        value={currentlyAttending}
+        keyboardType={'numeric'}
+      />
 
       <View style={styles.dropdownContainer}>
         <Text style={styles.text}>Skill Level:</Text>
         <ModalDropdown
+          renderButtonText={skillLevel => setSkillLevel(skillLevel)}
           textStyle={styles.dropdownText}
           dropdownTextStyle={styles.dropdownTextStyle}
           defaultValue='>> select skill level <<'
@@ -62,8 +109,21 @@ export default function CreateEventForm() {
         />
       </View>
 
-      <TextInput style={styles.input} placeholder='Equipment Reqired...'/>
-      <TextInput style={styles.input} placeholder='Notes...'/>
+      <TextInput
+        style={styles.input}
+        placeholder='Equipment Reqired...'
+        onChangeText={equipmentRequired => setEquipmentRequired(equipmentRequired)}
+        value={equipmentRequired}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder='Notes...'
+        onChangeText={notes => setNotes(notes)}
+        value={notes}
+      />
+
+
       <View style={styles.createEventButton}><Button onPress={validateInputs} title='create event!'/></View>
     </View>
   );
