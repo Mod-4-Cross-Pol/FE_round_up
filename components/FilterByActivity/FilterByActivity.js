583fc28f14026358ec009916003f4f8903c8257b
Zachary Nemeroff <zanemeroff@gmail.com>
2020-04-10 12:35:13 -0600

    Refactor styling

--- components/FilterByActivity/FilterByActivity.js
+++ components/FilterByActivity/FilterByActivity.js
@@ -11,26 +11,38 @@ export default function FilterByActivity(props) {
     props.setFilter(value)
   }
   return (
-    <View style={styles.dropdownContainer}>
-    <ModalDropdown
-      renderButtonText={activityFilterValue => setFilterValue(activityFilterValue)}
-      textStyle={styles.dropdownText}
-      dropdownTextStyle={styles.dropdownTextStyle}
-      defaultValue='By activity -'
-      options={[ 'Show all events', 'Baseball', 'Basketball', 'Biking', 'Bocce ball', 'Climbing', 'Croquet', 'Disc golf', 'Fishing', 'Football', 'Hand ball', 'Kickball', 'Outdoor exercising', 'Slacklining', 'Soccer', 'Spike ball', 'Tennis', 'Trail hiking/walking', 'Ultimate frisbee', 'Volleyball', 'Miscellaneous']}
-    />
-  </View>
+    <View style={styles.filterContainer}>
+      <Text style={styles.filterLabel}>Filter Events:</Text>
+      <ModalDropdown
+        renderButtonText={activityFilterValue => setFilterValue(activityFilterValue)}
+        textStyle={styles.dropdownText}
+        dropdownTextStyle={styles.dropdownTextStyle}
+        defaultValue='>> select activity <<'
+        options={[ 'Show all events', 'Baseball', 'Basketball', 'Biking', 'Bocce ball', 'Climbing', 'Croquet', 'Disc golf', 'Fishing', 'Football', 'Hand ball', 'Kickball', 'Outdoor exercising', 'Slacklining', 'Soccer', 'Spike ball', 'Tennis', 'Trail hiking/walking', 'Ultimate frisbee', 'Volleyball', 'Miscellaneous']}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  filterContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 5,
+    backgroundColor: '#28BFBD',
+    borderWidth: 4,
+    borderColor: '#292c33',
+    borderTopWidth: 0
+  },
+  filterLabel: {
+    fontSize: 22
+  },
   dropdownText: {
-    fontSize: 20,
-    backgroundColor: '#fff'
+    fontSize: 22
   },
   dropdownTextStyle: {
-    fontSize: 20,
-    width: 150
+    fontSize: 22,
+    width: 200
   },
   dropdownContainer: {
     alignItems: 'center',
