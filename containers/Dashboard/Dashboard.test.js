f4983af3150bc3c4d43bdeb1ab8afd04104028f7
Zachary Nemeroff <zanemeroff@gmail.com>
2020-04-15 17:58:56 -0600

    Add tests

--- containers/Dashboard/Dashboard.test.js
+++ containers/Dashboard/Dashboard.test.js
@@ -1,17 +1,24 @@
 import React from 'react';
-// import { Dashboard } from './Dashboard';
-import renderer from 'react-test-renderer';
+import { Dashboard } from './Dashboard';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Dashboard', () => {
 
   describe('Renders a Dashboard component', () => {
 
-    // test('renders correctly', () => {
-    //   const wrapper = renderer.create(<Dashboard />).toJSON();
-    //   expect(wrapper).toMatchSnapshot();
-    // });
+  let wrapper;
 
-    test('it works', () => {
+  beforeEach(() => {
+    wrapper = Enzyme.shallow(<Dashboard />);
+  })
+
+    test('renders the component', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    test('verfy testing file works', () => {
       expect(true).toBeTruthy();
     });
 
