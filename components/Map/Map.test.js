e52a0139b465b439c6f4822e9e5dca6e3a8be9aa
Zachary Nemeroff <zanemeroff@gmail.com>
2020-04-15 17:58:09 -0600

    Add tests

--- components/Map/Map.test.js
+++ components/Map/Map.test.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import { Map } from './Map';
-import renderer from 'react-test-renderer';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Map', () => {
 
   describe('Renders a Map component', () => {
 
-    // test('renders correctly', () => {
-    //   const wrapper = renderer.create(<Map />).toJSON();
-    //   expect(wrapper).toMatchSnapshot();
-    // });
+  let wrapper;
 
-    test('it works', () => {
+  beforeEach(() => {
+    const mockProps = {
+      events: [{
+        activity: 'Basketball',
+        attending: false,
+        current_participant_count: 1,
+        date: '2020-04-15',
+        description: '',
+        duration: '45 min',
+        equipment: 'fds',
+        id: 51,
+        lat_long: '37.064236,-95.6431662',
+        location: 'Park',
+        max_participant_count: 2,
+        skill_level: 'Just for fun!',
+        start_time: '6:00AM',
+      }]
+    }
+      wrapper = Enzyme.shallow(<Map {...mockProps}/>);
+    })
+
+    test('renders the component', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    test('verfy testing file works', () => {
       expect(true).toBeTruthy();
     });
 
