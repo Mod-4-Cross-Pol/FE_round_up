2435d1e2120563520784bbcf6a63417f0077316d
Ryan Novak <ryan.novak95@yahoo.com>
2020-04-15 15:08:37 -0600

    removed unused code within loginview component

--- components/LoginView/LoginView.js
+++ components/LoginView/LoginView.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-const Stack = createStackNavigator();
+import { StyleSheet, View, Button, TextInput, Image } from 'react-native';
 
 export function LoginView({ navigation }) {
   return (
